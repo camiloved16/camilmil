9a30a31f0b2c67b3d36a7220be2105b6b9f60aa9
camiloved16 <36846705+camiloved16@users.noreply.github.com>
2018-02-26 16:32:30 +0100

    Add files via upload

---
+++ Randomprova.java
@@ -0,0 +1,27 @@
+
+public class Randomprova {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+		int r = 0;
+		int sum1 = 0;
+
+		
+		
+		
+		while (r < 20) {
+			x= (int) (Math.random() * 100);
+
+			if ((x % 2) != 0) {
+
+				System.out.println("num.random dispari:  " + f);
+
+				sum1 += f;
+
+				System.out.println(" num.somma tot:  " + sum1);
+
+			}
+		}
+	}
+}
